fc73abdba6cafb3f4b97cb9e3bd90316d350817c
paolo coca <mrpaolo419@gmail.com>
2024-07-21 22:19:28 -0300

    proyecto

--- main.js
+++ main.js
@@ -1,79 +1,152 @@
-// const form = document.getElementById("ingreso1");
-// const tipo = document.getElementById("tipo");
-// const precio = document.getElementById("precio");
-// const color = document.getElementById("color");
-// const contenedor = document.getElementById("lista");
 
-// let guardarLista = [];
 
-// const getRandomId = () => {
-//     return Math.floor(1000 + Math.random() * 9000 ).toString();
-// };
+const menuTableBody = document.querySelector("#menuTableBody")
 
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault();   
-//     const id = getRandomId();
-//     const nTipo = tipo.value;
-//     const nPrecio = precio.value;
-//     const nColor = color.value;
+
+
+if (!localStorage.getItem("comida")) {
+localStorage.setItem("comida", JSON.stringify(menus));
+}
+
+function mostrarLista(regimenId) {
+    const menusGuardados = JSON.parse(localStorage.getItem("comida"));
+    const menusFiltrados = menusGuardados.filter(menu => menu.id === regimenId);
+    const menuTableBody = document.querySelector("#menuTableBody");
+    menuTableBody.innerHTML = "";
+menusFiltrados.forEach(menu => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${menu.id}</td>
+    <td>${menu.entrada}</td>
+    <td>${menu.principal}</td>
+    <td>${menu.postre}</td>
+    <td>${menu.regimen}</td>
+    `;
+    menuTableBody.appendChild(row);
+});
+}
+
+for (let i = 1; i <= 16; i++) {
+    const button = document.getElementById(`menu${i}`);
+    button.addEventListener('click', () => mostrarLista(i));
+}
+
+window.addEventListener('load', () => mostrarLista(1));
+
+
+const nombre = document.querySelector(".indi-nombre");
+const sala = document.querySelector(".indi-sala");
+const cama = document.querySelector(".indi-cama");
+const entrada = document.querySelector(".indi-entrada");
+const principal = document.querySelector(".indi-principal");
+const postre = document.querySelector(".indi-postre");
+const preparados = document.querySelector("#indiPreparados");
+
+const agregar = document.querySelector("#indi");
+let platosIndi = [];
+
+const getRandomId = () => {
+    return Math.floor(Math.random() * 9000) + 1000; 
+};
+
+agregar.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const idIndi = getRandomId();
+    const nombreIndi = nombre.value;
+    const salaIndi = sala.value;
+    const camaIndi = cama.value;
+    const entradaIndi = entrada.value;
+    const principalIndi = principal.value;
+    const postreIndi = postre.value;
     
-//     if (nTipo === "" || nPrecio ==="" || nColor ==="") {
-//         alert("Por favor, completa todos los campos.");
-//         return;
-//     }
-    
-//     const producto = agregar(id, nTipo, nPrecio, nColor);
-//     mandar(producto);
-//     guardarLista.push(producto); // Agregar producto a guardarLista
-//     guardar(guardarLista); // Guardar en localStorage
-//     form.reset();
-// });
+    const indiCreado = crearIndi(idIndi, nombreIndi, salaIndi, camaIndi, entradaIndi, principalIndi, postreIndi);
+    agregarIndi(indiCreado);
 
-// const agregar = (gId, gTipo, gPrecio, gColor) => {
-//     return {
-//         id: gId,
-//         Tipo: gTipo,
-//         Precio : gPrecio,
-//         Color: gColor 
-//     };
-// };
+    agregar.reset();
+});
 
-// const mandar = (producto) => {
-//     const div = document.createElement("div");
-//     div.className = "productos";
-//     div.id = `producto-${producto.id}`;
-//     div.innerHTML = `
-//         <div class="productos">
-//             <h4>Tipo: ${producto.Tipo}</h4>
-//             <p>Precio: ${producto.Precio}</p>
-//             <p>Color: ${producto.Color}</p>
-//             <button class="eliminar" id="${producto.id}">Eliminar</button>
-//         </div>
-//     `;
-//     const boton = div.querySelector(".eliminar");
-//     boton.addEventListener("click", () => {
-//         div.remove();
-//         eliminarProducto(producto.id); // Eliminar producto de guardarLista y localStorage
-//     });
-    
-//     contenedor.appendChild(div);
-// };
+const crearIndi = (indiId, indiNombre, indiSala, indiCama, indiPrincipal, indiPostre) => {
+    return {
+        id: indiId,
+        nombre: indiNombre,
+        sala: indiSala,
+        cama: indiCama,
+        principal: indiPrincipal,
+        postre: indiPostre
+    };
+};
 
-// const guardar = (lista) => {
-//     localStorage.setItem("guardarLista", JSON.stringify(lista));
-// };
+const agregarIndi = (indiCreado) => {
+    const row = document.createElement("tr");
 
-// const cargarDesdeLocalStorage = () => {
-//     const datos = localStorage.getItem("guardarLista");
-//     return datos ? JSON.parse(datos) : [];
-// };
+    row.innerHTML = `
+        <td>${indiCreado.nombre}</td>
+        <td>${indiCreado.sala}</td>
+        <td>${indiCreado.cama}</td>
+        <td>${indiCreado.entrada}</td>
+        <td>${indiCreado.principal}</td>
+        <td>${indiCreado.postre}</td>
+        <td><button class="eliminar" id="${indiCreado.id}">eliminar</button></td>
+    `;
+    preparados.appendChild(row);
 
-// guardarLista = cargarDesdeLocalStorage();
+    platosIndi.push(indiCreado);
+    guardarIndi(platosIndi);
+};
 
-// // Mostrar productos guardados desde localStorage al cargar la página
-// guardarLista.forEach(producto => mandar(producto));
+const verIndi = () => {
+    platosIndi = mostrarIndi(); 
+    preparados.innerHTML = "";
+    platosIndi.forEach((platoIndi) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${platoIndi.nombre}</td>
+            <td>${platoIndi.sala}</td>
+            <td>${platoIndi.cama}</td>
+            <td>${platoIndi.entrada}</td>
+            <td>${platoIndi.principal}</td>
+            <td>${platoIndi.postre}</td>
+            <td><button class="eliminar" id="${platoIndi.id}">eliminar</button></td>
+        `;
+        preparados.appendChild(row);
+    });
+};
 
-// const eliminarProducto = (id) => {
-//     guardarLista = guardarLista.filter(producto => producto.id !== id);
-//     guardar(guardarLista); // Actualizar localStorage
-// };
+document.addEventListener('DOMContentLoaded', verIndi);
+
+const eliminarIndi = (idindividual) => {
+    platosIndi = platosIndi.filter(indi => indi.id !== parseInt(idindividual));
+    verIndi(); 
+    guardarIndi(platosIndi); 
+};
+
+document.addEventListener('DOMContentLoaded', verIndi);
+
+preparados.addEventListener("click", (e) => {
+    if (e.target.classList.contains("eliminar")) {
+        eliminarIndi(e.target.id);
+    }
+});
+
+
+
+
+const guardarIndi = (platosIndi) => {
+    localStorage.setItem('platosIndi', JSON.stringify(platosIndi));
+};
+
+const mostrarIndi = () => {
+    const platosIndiJSON = localStorage.getItem("platosIndi");
+    if (platosIndiJSON) {
+        return JSON.parse(platosIndiJSON);
+    } else {
+        return []; 
+    }
+};
+
+const indiEstage = guardarIndi()
+
+
+
+
